c6b04a3000001e0584aeb50de0480852e6abe406
geekychetan <chetan.lohar@enterprisedb.com>
2024-07-21 15:25:36 +0530

    Inial Commit - Project setup

---
+++ src/main/java/com/geekychetan/springboot_crud_app/SpringbootCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.geekychetan.springboot_crud_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekychetan/springboot_crud_app/SpringbootCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekychetan.springboot_crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
